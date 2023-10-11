a1da967d22a8e921d31aa9055c45055223d929a4
saravannakumar <145510611+saravana-ar@users.noreply.github.com>
2023-10-11 18:02:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var element = document.createElement("div");
+element.setAttribute("class", "container");
+element.setAttribute("class", "row");
+element.setAttribute("class", "col");
+element.innerHTML = "this is tag";
+document.body.append(element);
